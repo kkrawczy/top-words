58ebb07da02f2df7046942f95952898a47e06541
karol <kkrawczyk.pl@gmail.com>
2015-06-12 22:27:44 +0200

    add simple table for words

--- js/app.js
+++ js/app.js
@@ -4,11 +4,46 @@
 
     var Test = React.createClass({
         render: function () {
-            return <div>
-                <h1>Hello React</h1>
-            </div>
+            var rows;
+            if (this.props.words.length > 0)
+                rows = this.props.words.map(function (word) {
+                    return <Row word={word}/>
+                });
+            else {
+                rows = <tr>
+                    <td colSpan="3">Sorry ... no data</td>
+                </tr>;
+            }
+            return <table className="table table-striped table-bordered">
+                <thead>
+                <tr>
+                    <th>English</th>
+                    <th>French</th>
+                    <th width="15%">Audio</th>
+                </tr>
+                </thead>
+                <tbody id="tbody">
+                {rows}
+                </tbody>
+            </table>
         }
     });
 
-    React.render(<Test/>, document.getElementById("app"));
-});
+    var Row = React.createClass({
+        render: function () {
+            return <tr>
+                <td>{this.props.word.english}</td>
+                <td>{this.props.word.french}</td>
+                <td></td>
+            </tr>;
+        }
+    });
+
+
+    var words = [{english: "Hello dude!", french: "Salut mec!"},
+        {english: "I would like to buy 3 wines.", french: "Je voudrais acheter 3 vins."},
+        {english: "borrow", french: "emprunter"},
+        {english: "to be", french: "être"}];
+
+    React.render(<Test words={words}/>, document.getElementById("app"));
+})();
