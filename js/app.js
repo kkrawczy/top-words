dac34f2f691f14307b4378a81a8d064945f31b69
karol <kkrawczyk.pl@gmail.com>
2015-06-14 15:53:09 +0200

    add dirty recording feature

--- js/app.js
+++ js/app.js
@@ -2,12 +2,35 @@
 (function () {
     'use strict';
 
+    try {
+        // webkit shim
+        window.AudioContext = window.AudioContext || window.webkitAudioContext;
+        navigator.getUserMedia = ( navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia);
+        window.URL = window.URL || window.webkitURL;
+
+        var audio_context = new AudioContext;
+        console.log('Audio context set up.');
+        console.log('navigator.getUserMedia ' + (navigator.getUserMedia ? 'available.' : 'not present!'));
+    } catch (e) {
+        console.log(e);
+        alert('No web audio support in this browser!');
+    }
+
+    var recorderObject;
+    navigator.getUserMedia({audio: true}, function (stream) {
+        recorderObject = new MP3Recorder(audio_context, stream);
+    }, function (e) {
+    });
+
     var Test = React.createClass({
         render: function () {
             var rows;
             if (this.props.words.length > 0)
-                rows = this.props.words.map(function (word) {
-                    return <Row word={word}/>
+                rows = this.props.words.map(function (word, i, xxx) {
+                    return <Row word={word} idKey={i}/>
                 });
             else {
                 rows = <tr>
@@ -22,7 +45,7 @@
                     <th width="15%">Audio</th>
                 </tr>
                 </thead>
-                <tbody id="tbody">
+                <tbody>
                 {rows}
                 </tbody>
             </table>
@@ -30,15 +53,59 @@
     });
 
     var Row = React.createClass({
+
         render: function () {
             return <tr>
                 <td>{this.props.word.english}</td>
                 <td>{this.props.word.french}</td>
-                <td></td>
+                <td><Recorder idKey={this.props.idKey}/></td>
             </tr>;
         }
     });
 
+    var Recorder = React.createClass({
+        stopIcon: "glyphicon glyphicon-stop",
+        recordIcon: "glyphicon glyphicon-record",
+
+        getInitialState: function () {
+            return {state: "stopped", icon: this.recordIcon}
+        },
+        handleRecord: function () {
+            if (this.state.state == "stopped") {
+                this.setState({state: "recording", icon: this.stopIcon});
+                recorderObject.init();
+                recorderObject.start();
+
+            } else {
+                this.setState({state: "stopped", icon: this.recordIcon});
+                recorderObject.stop();
+                var $this = this;
+                recorderObject.exportWAV(function (base64_wav_data) {
+                    var audioUrl = 'data:audio/wav;base64,' + base64_wav_data;
+                    console.log(audioUrl);
+                    $this.setState({audioUrl: audioUrl});
+                });
+            }
+        },
+        playAudio: function () {
+            var audio = document.getElementById(this.props.idKey);
+            audio.play();
+        },
+        render: function () {
+            var style = {
+                fontSize: 20,
+                marginRight: 10
+            };
+            console.log(this.props.idKey);
+
+            return <div>
+                <span className={this.state.icon} onClick={this.handleRecord} style={style}></span>
+                <audio id={this.props.idKey} src={this.state.audioUrl}></audio>
+                {this.state.audioUrl ?
+                    <span className="glyphicon glyphicon-play" onClick={this.playAudio} style={style}></span> : ""}
+            </div>
+        }
+    });
 
     var words = [{english: "Hello dude!", french: "Salut mec!"},
         {english: "I would like to buy 3 wines.", french: "Je voudrais acheter 3 vins."},
